831a1b7d68d467b20e7cc126b46a621659f617ac
snowgy920 <snowgy920@hotmail.com>
2021-11-27 00:13:05 +0800

    jsx sample

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,16 @@
-import './app.css';
+import "./app.css";
 
 function App() {
-  return <h1>Hello :)</h1>;
+  const name = undefined;
+  return (
+    <>
+      <h1>Hello</h1>
+      {name && <h1>Hello {name} :)</h1>}
+      {["🍎", "🍑"].map((item) => (
+        <h1>{item}</h1>
+      ))}
+    </>
+  );
 }
 
 export default App;
