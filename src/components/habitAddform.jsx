88864ff3403a05c810585b593b36cdc6ed81ff15
snowgy920 <snowgy920@hotmail.com>
2021-11-27 01:49:40 +0800

    4.13 complete using class components

---
+++ src/components/habitAddform.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class HabitAddform extends Component {
+  inputRef = React.createRef();
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    const name = this.inputRef.current.value;
+    name && this.props.onAdd(name);
+    this.inputRef.current.value = "";
+  };
+  render() {
+    return (
+      <form className='add-form' onSubmit={this.onSubmit}>
+        <input
+          ref={this.inputRef}
+          type='text'
+          className='add-input'
+          placeholder='Add your habit...'
+        />
+        <button className='add-button'>Add</button>
+      </form>
+    );
+  }
+}
+
+export default HabitAddform;
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,47 +1,28 @@
 import React, { Component } from "react";
 import Habit from "./habit";
+import HabitAddform from "./habitAddform";
 
 class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: "Reading", count: 0 },
-      { id: 2, name: "Running", count: 0 },
-      { id: 3, name: "Coding", count: 0 },
-    ],
-  };
-
-  handleIncrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
-    this.setState({ habits });
-  };
-  handleDecrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count > 0 && habits[index].count--;
-    this.setState({ habits });
-  };
-  handleDelete = (habit) => {
-    const habits = this.state.habits.filter((item) => item.id !== habit.id);
-    this.setState({ habits });
-
-    console.log(`handleIncrement ${habit.name}`);
-  };
-
   render() {
+    const habits = this.props.habits;
     return (
-      <ul>
-        {this.state.habits.map((habit) => (
-          <Habit
-            key={habit.id}
-            habit={habit}
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-            onDelete={this.handleDelete}
-          />
-        ))}
-      </ul>
+      <>
+        <HabitAddform onAdd={this.props.onAdd} />
+        <ul>
+          {habits.map((habit) => (
+            <Habit
+              key={habit.id}
+              habit={habit}
+              onIncrement={this.props.onIncrement}
+              onDecrement={this.props.onDecrement}
+              onDelete={this.props.onDelete}
+            />
+          ))}
+        </ul>
+        <button className='habits-reset' onClick={this.props.onReset}>
+          Reset All
+        </button>
+      </>
     );
   }
 }
