c4a1b657c0f6ecec0e0b565816555df0c316f348
snowgy920 <snowgy920@hotmail.com>
2021-11-27 00:29:05 +0800

    4.6 habit component 1

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,8 @@
 import "./app.css";
+import Habit from "./components/habit";
 
 function App() {
-  const name = undefined;
-  return (
-    <>
-      <h1>Hello</h1>
-      {name && <h1>Hello {name} :)</h1>}
-      {["🍎", "🍑"].map((item) => (
-        <h1>{item}</h1>
-      ))}
-    </>
-  );
+  return <Habit />;
 }
 
 export default App;
